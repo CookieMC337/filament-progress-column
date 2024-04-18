3fcc14f2f6b90fe6217151e6ceb39dc99d178675
CookieMC337 <51511368+CookieMC337@users.noreply.github.com>
2024-04-18 09:04:57 +0200

    Update column.blade.php

--- resources/views/column.blade.php
+++ resources/views/column.blade.php
@@ -6,14 +6,19 @@ $barStyles = \Filament\Support\get_color_css_variables(
 );
 $progress = $getProgress();
 $poll = $getPoll();
+$title = $getTitle();
+$usage_value = $getUsageValue();
+$free_value = $getFreeValue();
 @endphp
 
 <div
-    class="w-full filament-tables-progress-column"
+    class="w-full filament-tables-progress-column rounded-xl bg-white p-6 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
     @if($poll)
         wire:poll.{{ $poll }}
     @endif
 >
+    <h1 class="text-xl font-semibold text-gray-800 dark:text-white text-center">{{ $title }}</h1>
+
     <div class="flex items-center space-x-4 px-4">
         <div class="bg-gray-200 rounded-full h-2.5 dark:bg-gray-600 w-full">
             <div style="{{ $barStyles }}; width: {{ min($progress, 100) }}%" class="h-2.5 rounded-full bg-custom-600"></div>
